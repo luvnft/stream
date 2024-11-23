427fe9fd7e9f51ae3787ed954f0d6c577730bf58
The Wizard of Hahz <info@luvnft.com>
2024-11-23 12:24:30 -0500

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function IndexPage() {
       <div className="mx-auto flex w-full flex-col items-start gap-4">
         <div className="flex items-center gap-2">
           <h1 className="text-[48pt] leading-tight tracking-tighter text-black dark:text-black">
-            📺✨ HAHZ.live TV
+            📺HAHZ.live TV✨ 
           </h1>
         </div>
         <div className="flex flex-col gap-6 text-foreground">
